f432a3db7eb4e75f3b62e89085ce2291c43c6745
Admin <rgkoduri@gmail.com>
2018-11-09 15:40:01 +0530

    "Manage Subs Recycler Completed"

--- app/src/main/java/com/example/android/easyreddit/touchhelper/ItemTouchHelperAdapter.java
+++ app/src/main/java/com/example/android/easyreddit/touchhelper/ItemTouchHelperAdapter.java
@@ -3,7 +3,7 @@ package com.example.android.easyreddit.touchhelper;
 public interface  ItemTouchHelperAdapter {
 
 
-    void onItemMove(int fromPosition, int toPosition);
+    boolean onItemMove(int fromPosition, int toPosition);
 
-    boolean onItemDismiss(int position);
+    void  onItemDismiss(int position);
 }
--- app/src/main/java/com/example/android/easyreddit/ui/MangeSubreddit.java
+++ app/src/main/java/com/example/android/easyreddit/ui/MangeSubreddit.java
@@ -1,14 +1,49 @@
 package com.example.android.easyreddit.ui;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+
+import com.example.android.easyreddit.adapters.RedditRecyclierView;
 import com.example.android.easyreddit.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MangeSubreddit extends AppCompatActivity {
+
+    private static String LOG_TAG = MangeSubreddit.class.getSimpleName();
+    private LinearLayoutManager linearLayoutManager;
+    private RedditRecyclierView mRecyclerViewAdapter;
+    private ItemTouchHelper mItemTouchHelper;
+    final Context c = this;
+    SharedPreferences prefs;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.subList)
+    RecyclerView subredditList;
+
+
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mange_subreddit);
+        ButterKnife.bind(this);
     }
+
+
+
 }
